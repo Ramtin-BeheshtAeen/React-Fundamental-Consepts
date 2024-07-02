b6a796440fd8ee1279bba0a05e4ad6349964899a
Ramtin_BeheshtAeen_Laptop <ramtinba145822@@gmail.com>
2024-07-02 18:30:16 +0200

    exercise of the JSX Immediately invoked function

---
+++ src/components/JSX_Immediately_invoked_function.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function JSX_Immediately_invoked_function() {
+    let points =99;
+
+    return (
+        <div>
+    
+          {(
+    
+            () => {
+    
+              /* Functions Body: */
+              if(points>=80 && points<100){
+                return <h1>A+</h1>
+              }
+              else if(points>=70 && points<80){
+                return <h1>A-</h1>
+              }
+              else if(points>=60 && points<70){
+                return <h1>B</h1>
+              }
+              else {
+                return <h1>Failed</h1>
+              }
+    
+            }
+    
+            /* Calling the function in this section:*/
+          ) ()}
+
+        </div>
+      );
+};
+
+export default JSX_Immediately_invoked_function;
