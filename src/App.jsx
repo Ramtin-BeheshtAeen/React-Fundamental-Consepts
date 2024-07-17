2ba29d9f72dc2346fe359a38e350715292417bce
Ramtin_BeheshtAeen_Laptop <Ramtinba145822@gmail.com>
2024-07-17 21:45:47 +0200

    completing 'Passing Properties to Child Component s ection'

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,22 @@ import JSX_Conditional_Rendering_Using_switch from './components/JSX_Conditional
 import JSX_Conditional_Rendering_Using_Ternary_Operator from './components/JSX_Conditional_Rendering_Using_Ternary_Operator';
 import JSX_Conditional_Rendering_Using_And_And_Operator from './components/JSX_Conditional_Rendering_Using_And_And_Operator';
 import JSX_Passing_Properties_String_to_This_Child_Component from './components/JSX_Passing_Properties_String_to_This_Child_Component';
+import JSX_Passing_Properties_Object_to_This_Child_Component from './components/JSX_Passing_Properties_Object_to_This_Child_Component';
+import JSX_Passing_Properties_Function_to_This_Child_Component from './components/JSX_Passing_Properties_Function_to_This_Child_Component';
+
+
+function buttonOnClick(){
+  alert("You have clicked the button")
+}
 
 function App() {
+
+  const carObject = {
+    brand: "Volvo",
+    countryOfOrigin: "Sweden",
+    productionDate: 1927
+  }
+
   return (
     <div>
     <JSX_Immediately_invoked_function/>
@@ -26,7 +40,11 @@ function App() {
 
     <JSX_Passing_Properties_String_to_This_Child_Component 
     message='This is a String from parent component'/>
+
+    <JSX_Passing_Properties_Object_to_This_Child_Component car={carObject}/>
     
+    <JSX_Passing_Properties_Function_to_This_Child_Component func={buttonOnClick}/>
+
     </div>
   );
 };
