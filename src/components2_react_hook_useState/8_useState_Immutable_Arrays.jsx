fd53a4643d0de890a7df12e6383bb1a82ed53f69
Ramtin_BeheshtAeen_Laptop <Ramtinba145822@gmail.com>
2024-07-25 12:46:01 +0200

    add 'Immutable Arrays (Adding to the Array)'

--- src/components2_react_hook_useState/8_useState_Immutable_Arrays.jsx
+++ src/components2_react_hook_useState/8_useState_Immutable_Arrays.jsx
@@ -15,7 +15,25 @@ function UseState_Immutable_Arrays(){
 
     return (
         <div>
-            <h1>UseState_Immutable_Arrays</h1>
+            <h1>UseState_Immutable_Arrays_adding</h1>
+            <table>
+                <tbody>
+                    { list.length!==0 
+                      ? (
+                            list.map((element,index) => (
+                                <tr key={index}>
+                                    <td>{element}</td>
+                                </tr>
+                            ))
+                        ) 
+                      : (
+                            <tr>
+                                <td> Add Some Thing </td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
             <input placeholder='item' onChange={(event)=>(setItem(event.target.value))}/> 
             <button onClick={addItemToList}> add </button>
             <button onClick={showArray}> show </button>
